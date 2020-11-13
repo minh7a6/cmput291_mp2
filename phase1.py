d021f9d145650136953074a999b721511948835c
Le Minh Dang <mldang@cs.ualberta.ca>
2020-11-12 22:08:37 -0700

    good phase 1

--- phase1.py
+++ phase1.py
@@ -1,9 +1,15 @@
 import json
 from pymongo import MongoClient
+from tqdm import tqdm
 import sys
+from os import path
 def phase1(args):
-    if len(args) < 4:
+    if len(args) != 4:
         sys.exit("Need all 3 of the files")
+    for i in args[1:]:
+        if not path.isfile(i):
+            ret = "{0} is not a file".format(i)
+            sys.exit(ret)
     port = input("Please input your port number, we will try to connect using 'mongodb://localhost:port': ")
     url = "mongodb://localhost:" + str(port)
     client = MongoClient(url)
@@ -17,11 +23,12 @@ def phase1(args):
         db.Votes.drop()
     for i in args[1:]:
         collection = None
-        if i == "Posts.json":
+        arg = i.split("/")
+        if arg[-1] == "Posts.json":
             collection = db["Posts"]
-        elif i == "Tags.json":
+        elif arg[-1] == "Tags.json":
             collection = db["Tags"]
-        elif i == "Votes.json":
+        elif arg[-1] == "Votes.json":
             collection = db["Votes"]
         with open(i) as file: 
             file_data = json.load(file)
@@ -31,7 +38,16 @@ def phase1(args):
                 break
             for j in file_data[i]:
                 break
-            for k in file_data[i][j]:
-                collection.insert_one(k)
+            progress = 0
+            if(len(file_data[i][j]) < 500000):
+                collection.insert(file_data[i][j])
+            else:
+                while(progress < len(file_data[i][j])):
+                    if(progress > len(file_data[i][j]) - 500000):
+                        k = file_data[i][j][progress:]
+                    else:
+                        k = file_data[i][j][progress:progress + 500000]
+                    collection.insert(k)
+                    progress = progress + 500000
 if __name__ == "__main__":
     phase1(sys.argv)
