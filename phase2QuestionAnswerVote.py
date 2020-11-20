579882baad11f940e1c1e221b2dceeac1a7be3bd
Zi Xue Lim <zixue@cs.ualberta.ca>
2020-11-20 15:30:05 -0700

    number 5 kinda done - need testing

---
+++ phase2QuestionAnswerVote.py
@@ -0,0 +1,54 @@
+from pymongo import MongoClient
+import datetime
+def createId(db):
+    empty = False
+    counter =1
+    while(empty == False):
+        if(db.Votes.find_one({"Id":str(counter)}) == None):
+            empty =True
+            break
+        counter = counter +1
+    return counter
+def vote(db,pid,uid):
+    print("Vote page")
+    checkVote = False
+    if uid != "":
+        checkVote = db.Votes.find_one({"$and": [{"VoteTypeId":"2"},{"UserId": uid},{"PostId":pid}]}) == None # true if user has not voted on post
+        print(db.Votes.find_one({"$and": [{"VoteTypeId":"2"},{"UserId": uid},{"PostId":pid}]}) == None)
+    if(checkVote == True  or uid ==""):
+        today = datetime.datetime.now()
+        id = str(createId(db))
+        vote = {}
+        if(uid ==""):
+            vote = {
+            "Id": id,
+            "PostId": pid,
+            "VoteTypeId": "2",
+            "CreationDate": str(today)
+        }
+        else:
+            vote = {
+                "Id": id,
+                "PostId": pid,
+                "VoteTypeId": "2",
+                "UserId": uid,
+                "CreationDate": str(today)
+            }
+        votesCollection = db["Votes"]
+        votesCollection.insert(vote)
+        #print(db.Votes.find_one({"$and": [{"Id":id},{"UserId": uid}]}))
+        #db.Votes.find_one({"Id":pid})
+        score = db.Posts.find_one({"Id":pid})["Score"]+1
+        #print(db.Posts.find_one({"Id":pid})["Score"])
+        db.Posts.update({"Id":pid},{"$set":{"Score":score}})
+        #print(db.Posts.find_one({"Id":pid}))
+    else:
+        print("You have already voted for this post")
+
+if __name__ == "__main__":
+    url = "mongodb://localhost:" + str(50001)
+    client = MongoClient(url)
+    db = client["291db"]
+    pid = "1"
+    uid = ""
+    vote(db,pid,uid)
