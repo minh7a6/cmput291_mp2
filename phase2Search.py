dc660201838967887eb3b09a4afd54a7907f80ec
Le Minh Dang <mldang@cs.ualberta.ca>
2020-11-20 19:20:20 -0700

    half ass finished searching

--- phase2Search.py
+++ phase2Search.py
@@ -4,13 +4,14 @@ def SearchQuestion(db):
     keyword_arr = input("Please enter your keywords for searching the Question: ")
     # keyword = keyword_arr.split(" ")
     postColl = db["Posts"]
-    postColl.create_index(["", ])
-    regexStr = 
-    db.Posts.find({"PostTypeId": "1"}, {"Title": ""})
+    x = db.Posts.find({"PostTypeId": "1", "$text": {"$search": keyword_arr}}, {"_id": 0, "Title": 1, "CreationDate": 1, "Score": 1, "AnswerCount": 1})
+    for i in x:
+        print(i)
     print("No || Title || Creation Date || Score || Ans Count")
 
 
 def func_test():
     url = "mongodb://localhost:" + str(50001)
     client = MongoClient(url)
+    SearchQuestion(client["291db"])
 func_test()
